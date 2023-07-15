08b00ad3de0446573876b006d51ac57ba012e2a6
Виталий Глазков <viusglazkov@yandex.ru>
2023-07-15 22:23:04 +0500

    the secret key is added to the environment variable9

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'django_default_secret_key')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
